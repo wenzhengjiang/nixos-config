15f646e41b619a1dca8a8a8677409b2abaf5b919
Wenzheng Jiang <jwzh.hi@gmail.com>
2017-10-22 00:16:21 +1100

    Add autojump; dropbox; htop

--- packages-configuration.nix
+++ packages-configuration.nix
@@ -4,9 +4,12 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    autojump
+    dropbox
     networkmanagerapplet
     git
     google-chrome
+    htop
     tmux
     xclip
     vim_configurable 
